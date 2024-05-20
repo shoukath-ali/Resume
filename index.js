f23b0734c1c74ba139260e062ece846033f9acf9
Shoukath Ali Shaik <shoukath.vizag@gmail.com>
2024-05-20 19:14:28 -0400

    final_update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 for (i=0;i<6;i++){
     document.querySelectorAll('.nav-link')[i].addEventListener("click",function(){
-        var  a = new Audio('sounds/tom-3.mp3');
+        var  a = new Audio('sounds/f1.mp3');
         a.play();
     });
 };
